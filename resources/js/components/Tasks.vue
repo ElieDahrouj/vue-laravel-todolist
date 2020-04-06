b573ae4580b912157053492fe8a226060168740b
elie <edarouj@hotmail.com>
2020-04-06 19:19:14 +0200

    add secu

--- resources/js/components/Tasks.vue
+++ resources/js/components/Tasks.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="container d-flex flex-wrap justify-content-center">
-        <div v-if="tasks.length !== 0" v-for="task in tasks" :key="task.id" class="card m-2" style="width: 18rem;">
-            <div class="card-body d-flex flex-column justify-content-between">
-                <p class="card-text">{{task.description}}</p>
-                <div class="d-flex justify-content-start">
-                    <router-link v-bind:to="'/hello/'+task.id"><button class="btn btn-dark rounded mr-2">More</button></router-link>
-                    <router-link v-bind:to="'/tasks/'+task.id+'/edit'"><button class="btn btn-light rounded mr-2">Update</button></router-link>
-                    <button @click="deleteTask(task.id)" class="btn btn-danger rounded">Delete</button>
+    <div>
+        <div v-if="tasks.length !== 0" class="container d-flex flex-wrap justify-content-center">
+            <div v-for="task in tasks" :key="task.id" class="card m-2" style="width: 18rem;">
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <p class="card-text">{{task.description}}</p>
+                    <div class="d-flex justify-content-start">
+                        <router-link v-bind:to="'/hello/'+task.id"><button class="btn btn-dark rounded mr-2">More</button></router-link>
+                        <router-link v-bind:to="'/tasks/'+task.id+'/edit'"><button class="btn btn-light rounded mr-2">Update</button></router-link>
+                        <button @click="deleteTask(task.id)" class="btn btn-danger rounded">Delete</button>
+                    </div>
                 </div>
             </div>
         </div>
