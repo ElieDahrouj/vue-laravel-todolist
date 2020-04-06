bb15f80419cdb0a97e42db599ad871ed095c128f
elie <edarouj@hotmail.com>
2020-04-06 17:36:51 +0200

    modif post

--- resources/js/views/Create.vue
+++ resources/js/views/Create.vue
@@ -44,7 +44,7 @@
                     this.success = false
                 }
                 if(this.$v.description.required){
-                    axios.post('http://api/tasks',{description: this.description})
+                    axios.post('/api/tasks',{description: this.description})
                         .then(res =>{
                             this.success = true
                             this.msgSuccess = res.data
